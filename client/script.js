d3319991949bc113f6a8879f6ebb7185f4318043
Laurent Clouet <laurent35240@gmail.com>
2016-08-09 15:25:52 +0200

    Starting to turn ON Hi-Fi through react interface

--- client/script.js
+++ client/script.js
@@ -1,8 +1,24 @@
-var Power = React.createClass({
-    render: function() {
-        return <a href="#" className="btn btn-lg btn-default"><span className="text-success glyphicon glyphicon-off" aria-label="power"></span></a>;
+class Power extends React.Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
     }
-});
+    handleClick() {
+        fetch("http://localhost:8080/power", {
+            method: "PUT",
+            body: {
+                state: "ON"
+            }
+        })
+    }
+    render() {
+        return (
+            <a href="#" className="btn btn-lg btn-default" onClick={this.handleClick}>
+                <span className="text-success glyphicon glyphicon-off" aria-label="power"></span>
+            </a>
+        );
+    }
+}
 
 ReactDOM.render(
 <Power name="World" />,
